2b8fdb71b2285c0ef839311f61f3cf56a20034db
Renzo Poggio <Renzo.martin904@gmail.com>
2020-11-19 17:55:56 -0300

    Added functions

--- Bot/main.py
+++ Bot/main.py
@@ -5,12 +5,17 @@ import os
 
 
 client = commands.Bot(command_prefix="!")
-token=os.getenv("BOT_TOKEN")
+token=os.getenv("DISCORD_BOT_TOKEN")
+
+@client.event
+async def on_ready():
+    print("Bot started")
+
 
 @client.command()
 async def welcome(ctx):
-    await ctx.send("holis")
-            
+    await ctx.send("holis") 
+
 
 client.run(token)
 
