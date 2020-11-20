cb0efe59092db1acb6e6bb998e9460f8c8902eb1
Renzo Poggio <Renzo.martin904@gmail.com>
2020-11-19 22:01:59 -0300

    Changes

--- botfiles/main.py
+++ botfiles/main.py
@@ -1,10 +1,8 @@
 import discord
-from discord import channel
 from discord.ext import commands
 import os
 
-
-client = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix="!", description="Bot made it by Renzo")
 token=os.getenv("DISCORD_BOT_TOKEN")
 
 @client.event
@@ -12,10 +10,25 @@ async def on_ready():
     print("Bot started")
 
 
-@client.command()
-async def welcome(ctx):
-    await ctx.send("holis") 
+@client.command(pass_context=True)
+async def com(ctx, arg : str=None):
+    if arg is None:
+        print("empty argument")
+    elif arg == "hi":
+        await ctx.send("Hi!")
 
+
+@client.command()
+async def playing(ctx,playing : str=None):
+    game = discord.Game(playing)
+    await client.change_presence(activity=game)
+
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        return await ctx.send("`Comando desconocido`")
+    raise error
 
 client.run(token)
 
