0d3936f3883e6f65b4ddf14f2b4615f9e60a5503
Renzo Poggio <Renzo.martin904@gmail.com>
2020-11-20 21:57:44 -0300

    One-Argument

--- botfiles/activity.py
+++ botfiles/activity.py
@@ -7,16 +7,16 @@ class bot_activity(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def watching(self, ctx,video : str=None):
+    async def watching(self, ctx, *, video : str=None):
         video = discord.Activity(name=video, type=0)
         await self.bot.change_presence(activity=video)
 
     @commands.command()
-    async def playing(self, ctx,playing : str=None):
+    async def playing(self, ctx, *, playing : str=None):
         game = discord.Activity(name=playing, type=1)
         await self.bot.change_presence(activity=game)
     
     @commands.command()
-    async def listening(self, ctx,listening : str=None):
+    async def listening(self, ctx, *, listening : str=None):
         music = discord.Activity(name=listening, type=2)
         await self.bot.change_presence(activity=music)
