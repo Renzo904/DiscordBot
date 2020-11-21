fda6c19f137213ba3292c97a631788f1fcd615c4
Renzo Poggio <Renzo.martin904@gmail.com>
2020-11-20 21:49:29 -0300

    Bot activity commands

---
+++ botfiles/activity.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+class bot_activity(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def watching(self, ctx,video : str=None):
+        video = discord.Activity(name=video, type=0)
+        await self.bot.change_presence(activity=video)
+
+    @commands.command()
+    async def playing(self, ctx,playing : str=None):
+        game = discord.Activity(name=playing, type=1)
+        await self.bot.change_presence(activity=game)
+    
+    @commands.command()
+    async def listening(self, ctx,listening : str=None):
+        music = discord.Activity(name=listening, type=2)
+        await self.bot.change_presence(activity=music)
--- botfiles/main.py
+++ botfiles/main.py
@@ -1,31 +1,37 @@
 import discord
-from discord.ext import commands
 import os
+from discord.ext import commands
+from discord.ext import tasks
+from activity import bot_activity
 
-client = commands.Bot(command_prefix="!", description="Bot made it by Renzo")
-token=os.getenv("DISCORD_BOT_TOKEN")
 
-@client.event
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix = "!", intents = intents, description = "Bot made it by Renzo")
+token = os.getenv("DISCORD_BOT_TOKEN")
+
+@bot.event
 async def on_ready():
     print("Bot started")
 
+@bot.event
+async def on_member_join(member):
+    channel = discord.utils.get(member.guild.channels, name="general")
+    await channel.send(f"Bienvenido al servidor {member.mention}!!")
+    print(f"Se ha unido el miembro {member.name}")
+    
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def com(ctx, arg : str=None):
     await ctx.send("Hi!")
 
 
-@client.command()
-async def playing(ctx,playing : str=None):
-    game = discord.Game(playing)
-    await client.change_presence(activity=game)
-
-
-@client.event
+@bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         return await ctx.send("`Comando desconocido`")
     raise error
 
-client.run(token)
+bot.add_cog(bot_activity(bot))
+bot.run(token)
 
